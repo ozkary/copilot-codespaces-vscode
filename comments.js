61e53538941be5b5782967ec345d6715c5b8c9dc
Oscar D. Garcia <ozkary@yahoo.com>
2023-07-20 19:12:53 +0000

    add comments.js

--- comments.js
+++ comments.js
@@ -1,87 +1,56 @@
 // create web server with express
 const express = require('express');
 const app = express();
+const path = require('path');
+const PORT = 3000;
+
+// connect to database
+const db = require('../database/index.js');
+
+// middleware
 const bodyParser = require('body-parser');
-const expressSanitizer = require('express-sanitizer');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-
-// connect to mongodb
-mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true });
-// set view engine
-app.set('view engine', 'ejs');
-// set public directory
-app.use(express.static('public'));
-// use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
-// use express-sanitizer
-app.use(expressSanitizer());
-// use method-override
-app.use(methodOverride('_method'));
+app.use(bodyParser.json());
 
-// Mongoose/Model Config
-const blogSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    body: String,
-    created: {
-        type: Date, 
-        default: Date.now
+// serve static files
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// routes
+app.get('/comments', (req, res) => {
+  db.getComments((err, data) => {
+    if (err) {
+      console.log('error retrieving comments');
+      res.status(404).send();
+    } else {
+      console.log('retrieved comments');
+      res.status(200).send(data);
     }
-});
-const Blog = mongoose.model('Blog', blogSchema);
-
-// RESTful Routes
-app.get('/', (req, res) => {
-    res.redirect('/blogs');
+  });
 });
 
-// INDEX ROUTE
-app.get('/blogs', (req, res) => {
-    Blog.find({}, (err, blogs) => {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render('index', { blogs: blogs });
-        }
-    });
+app.post('/comments', (req, res) => {
+  db.postComment(req.body, (err) => {
+    if (err) {
+      console.log('error posting comment');
+      res.status(404).send();
+    } else {
+      console.log('posted comment');
+      res.status(201).send();
+    }
+  });
 });
 
-// NEW ROUTE
-app.get('/blogs/new', (req, res) => {
-    res.render('new');
+app.delete('/comments', (req, res) => {
+  db.deleteComment(req.body, (err) => {
+    if (err) {
+      console.log('error deleting comment');
+      res.status(404).send();
+    } else {
+      console.log('deleted comment');
+      res.status(204).send();
+    }
+  });
 });
 
-// CREATE ROUTE
-app.post('/blogs', (req, res) => {
-    // create blog
-    req.body.blog.body = req.sanitize(req.body.blog.body);
-    Blog.create(req.body.blog, (err, newBlog) => {
-        if(err) {
-            res.render('new');
-        } else {
-            // redirect to the index
-            res.redirect('/blogs');
-        }
-    });
-});
-
-// SHOW ROUTE
-app.get('/blogs/:id', (req, res) => {
-    // find blog with provided ID
-    Blog.findById(req.params.id, (err, foundBlog) => {
-        if(err) {
-            // redirect to the index
-            res.redirect('/blogs');
-        } else {
-            // render show template with that blog
-            res.render('show', { blog: foundBlog });
-        }
-    });
-});
-
-// // EDIT ROUTE
-// app.get('/blogs/:id/edit', (req, res) => {
-//     // find
-
-// }
+// start server
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
